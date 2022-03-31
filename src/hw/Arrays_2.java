c82597030fa3e5f759c1e58e165ab1d665366a3b
Vladimir Kalinin <Vofanchic@mail.ru>
2022-03-31 19:40:01 +0500

    1-6 arr2 added

--- src/hw/Arrays_2.java
+++ src/hw/Arrays_2.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Arrays_2 {
     public static void main(String[] args) {
         int n = 5;
-        int m =6;
-        int a[][] = inputArray(n,m, 0,10000);
+        int m =5;
+        int a[][] = inputArray(n,m, 0,4);
         printArray(a,n,m);
-        System.out.println(task1(a,n,m));
+//        System.out.println(task2(a,n,m));
+        task6(a,n,m);
 
     }
 
@@ -22,6 +23,74 @@ public class Arrays_2 {
 
     }}
         return a;}
+
+    public static int task2(int arr[][], int n, int m) {
+        int a = arr[0][0];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j]>a){
+                    a=arr[i][j];
+                }
+
+            }}
+        return a;}
+
+    public static void task3(int arr[][], int n, int m) {
+        int a = arr[0][0];
+        int maxb=0;
+        int maxc=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j]>a){
+                    a=arr[i][j];
+                    maxb = i;
+                    maxc = j;
+                }
+
+            }}
+        System.out.println("["+maxb+"]"+"["+maxc+"]");}
+
+    public static void task4(int arr[][], int n, int m) {
+        int a = arr[0][0];
+        int maxb=0;
+        int maxc=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j]<a){
+                    a=arr[i][j];
+                    maxb = i;
+                    maxc = j;
+                }
+
+            }}
+        System.out.println("["+maxb+"]"+"["+maxc+"]");}
+
+    public static void task5(int arr[][], int n, int m) {
+        int a,b,c;
+        int caunt=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (j-1<0){a=0;} else a = arr[i][j-1];
+                if (j+1==m){c=0;} else c = arr[i][j+1];
+                b=arr[i][j];
+                if(b>a+c){
+                    caunt++;
+                }
+            }}
+        System.out.println(caunt);}
+
+    public static void task6(int arr[][], int n, int m) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < m; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        System.out.println();
+        printArray(arr,n,m);
+    }
 
     public  static int[][] inputArray(int n, int m, int a, int b) {
         int arr[][] = new int[n][m];
