d8913ab1b3ac26ac19a75fbb561569cd5bf56234
Vladimir Kalinin <Vofanchic@mail.ru>
2022-04-01 16:50:41 +0500

    6-9 added

--- src/hw/Arrays_2.java
+++ src/hw/Arrays_2.java
@@ -4,14 +4,14 @@ import java.util.Random;
 
 public class Arrays_2 {
     public static void main(String[] args) {
-        int n = 4;
-        int m =5;
-//        int a[][] = inputArray(n,m, 0,2);
-        int a[][] = {{0,0,0,0,1},{0,1,0,1,0},{0,0,0,0,0},{0,5,4,0,0}};
+        int n = 3;
+        int m = 3;
+        int a[][] = inputArray(n, m, 10, 100);
+//        int a[][] = {{5, 4, 6}, {10, 3, 8},  {0, 2, 0}};
 
-        printArray(a,n,m);
+        printArray(a, n, m);
 //        System.out.println(task2(a,n,m));
-        task72(a,n,m);
+        task9(a, n, m);
 
     }
 
@@ -19,111 +19,211 @@ public class Arrays_2 {
         int a = arr[0][0];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j]<a){
-                    a=arr[i][j];
+                if (arr[i][j] < a) {
+                    a = arr[i][j];
                 }
 
-    }}
-        return a;}
+            }
+        }
+        return a;
+    }
 
     public static int task2(int arr[][], int n, int m) {
         int a = arr[0][0];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j]>a){
-                    a=arr[i][j];
+                if (arr[i][j] > a) {
+                    a = arr[i][j];
                 }
 
-            }}
-        return a;}
+            }
+        }
+        return a;
+    }
 
     public static void task3(int arr[][], int n, int m) {
         int a = arr[0][0];
-        int maxb=0;
-        int maxc=0;
+        int maxb = 0;
+        int maxc = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j]>a){
-                    a=arr[i][j];
+                if (arr[i][j] > a) {
+                    a = arr[i][j];
                     maxb = i;
                     maxc = j;
                 }
 
-            }}
-        System.out.println("["+maxb+"]"+"["+maxc+"]");}
+            }
+        }
+        System.out.println("[" + maxb + "]" + "[" + maxc + "]");
+    }
 
     public static void task4(int arr[][], int n, int m) {
         int a = arr[0][0];
-        int maxb=0;
-        int maxc=0;
+        int maxb = 0;
+        int maxc = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j]<a){
-                    a=arr[i][j];
+                if (arr[i][j] < a) {
+                    a = arr[i][j];
                     maxb = i;
                     maxc = j;
                 }
 
-            }}
-        System.out.println("["+maxb+"]"+"["+maxc+"]");}
+            }
+        }
+        System.out.println("[" + maxb + "]" + "[" + maxc + "]");
+    }
 
     public static void task5(int arr[][], int n, int m) {
-        int a,b,c;
-        int caunt=0;
+        int a, b, c;
+        int caunt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (j-1<0){a=0;} else a = arr[i][j-1];
-                if (j+1==m){c=0;} else c = arr[i][j+1];
-                b=arr[i][j];
-                if(b>a+c){
+                if (j - 1 < 0) {
+                    a = 0;
+                } else a = arr[i][j - 1];
+                if (j + 1 == m) {
+                    c = 0;
+                } else c = arr[i][j + 1];
+                b = arr[i][j];
+                if (b > a + c) {
                     caunt++;
                 }
-            }}
-        System.out.println(caunt);}
+            }
+        }
+        System.out.println(caunt);
+    }
 
     public static void task6(int arr[][], int n, int m) {
 
         for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < m; j++) {
+            for (int j = i + 1; j < m; j++) {
                 int temp = arr[i][j];
                 arr[i][j] = arr[j][i];
                 arr[j][i] = temp;
             }
         }
         System.out.println();
-        printArray(arr,n,m);
+        printArray(arr, n, m);
     }
+
     public static void task71(int arr[][], int n, int m) {
-        boolean projArr[]= new boolean[m];
-        boolean unit=true;
+        boolean projArr[] = new boolean[m];
+        boolean unit = true;
         for (int i = 0; i < m; i++) {
-            unit=true;
+            unit = true;
             for (int j = 0; j < n; j++) {
-                if (arr[j][i]!=0)unit=false;
+                if (arr[j][i] != 0) unit = false;
             }
-            projArr[i]=unit;
+            projArr[i] = unit;
 
         }
-        printArrayBool(projArr,m);}
+        printArrayBool(projArr, m);
+    }
 
     public static void task72(int[][] arr, int n, int m) {
-        boolean projArr[]= new boolean[n];
-        boolean unit=false;
+        boolean projArr[] = new boolean[n];
+        boolean unit = false;
         for (int i = 0; i < n; i++) {
-            unit=false;
+            unit = false;
             for (int j = 1; j < m; j++) {
-                if (arr[i][j]>=arr[i][j-1])unit=true;
+                if (arr[i][j] <= arr[i][j - 1]) unit = true;
                 else {
-                    unit=false;
+                    unit = false;
                     break;
                 }
             }
-            projArr[i]=unit;
+            projArr[i] = unit;
 
         }
-        printArrayBool(projArr,n);}
+        printArrayBool(projArr, n);
+    }
 
-    public  static int[][] inputArray(int n, int m, int a, int b) {
+    public static void task73(int[][] arr, int n, int m) {
+        boolean projArr[] = new boolean[n];
+        boolean unit = false;
+        int p;
+        for (int i = 0; i < n; i++) {
+            unit = false;
+
+            for (int j = 0; j < m / 2; j++) {
+                if (arr[i][j] == arr[i][m - 1 - j]) unit = true;
+                else {
+                    unit = false;
+                    break;
+                }
+            }
+            projArr[i] = unit;
+
+        }
+        printArrayBool(projArr, n);
+    }
+
+    public static void task81(int[][] arr, int n, int m) {
+        int[] projArr = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = 0;
+            for (int j = 0; j < m; j++) {
+                sum += arr[i][j];
+            }
+            projArr[i] = sum;
+        }
+        printArray1(projArr, n);
+    }
+
+    public static void task82(int[][] arr, int n, int m) {
+        int[] projArr = new int[n];
+        int sum;
+        for (int i = 0; i < n; i++) {
+            sum = 1;
+            for (int j = 0; j < m; j++) {
+                sum *= arr[i][j];
+            }
+            projArr[i] = sum;
+        }
+        printArray1(projArr, n);
+    }
+
+    public static void task83(int[][] arr, int n, int m) {
+        double[] projArr = new double[n];
+        double sum;
+        for (int i = 0; i < n; i++) {
+            sum = 0;
+            for (int j = 0; j < m; j++) {
+                sum += arr[i][j];
+            }
+            sum/=m;
+            projArr[i] = sum;
+        }
+        printArrayDouble(projArr, n);
+    }
+
+    public static void task9(int[][] arr, int n, int m) {
+        int a=arr[0][0];
+        int min,minn,minm;
+
+        for (int i = 0; i < n; i++) {
+            min=arr[i][0];
+//            System.out.println(min);
+            minm=0;
+            minn=i;
+            for (int j = 1; j < m; j++) {
+                if (arr[i][j]<min){min=arr[i][j];minm=j;minn=i;}
+            }
+            for (int j = 0; j < n; j++) {
+                if (arr[j][minm]>min){break;}
+                else if (j==n-1){System.out.println(min+"["+minn+"]"+"["+minm+"]"); return;}
+            }
+        }
+        System.out.println("Не существует");
+
+    }
+
+
+
+    public static int[][] inputArray(int n, int m, int a, int b) {
         int arr[][] = new int[n][m];
         Random random = new Random();
         for (int i = 0; i < n; i++) {
@@ -145,8 +245,22 @@ public class Arrays_2 {
             System.out.println();
         }
     }
-    public static void printArrayBool(boolean[] a, int n){
-        for (int i = 0; i < n; i++){
+
+    public static void printArrayBool(boolean[] a, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + ",");
+        }
+        System.out.println();
+    }
+
+    public static void printArray1(int[] a, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + ",");
+        }
+        System.out.println();
+    }
+    public static void printArrayDouble(double[] a, int n) {
+        for (int i = 0; i < n; i++) {
             System.out.print(a[i] + ",");
         }
         System.out.println();
